9685ab03eb1848cb8567e99bad19c53d12d1ef20
nblasd <gameranonymous338@gmail.com>
2023-04-16 20:27:24 +0300

    Updated project with new features

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -36,16 +36,16 @@ function Navbar() {
           </h1>
         </Link>
         <ul style={{ color: `${textColor}` }} className="hidden sm:flex">
-          <li className="p-4 rounded-lg hover:bg-black">
+          <li className="p-4 rounded-lg hover:bg-gray-400">
             <Link href="/">Home</Link>
           </li>
-          <li className="p-4 rounded-lg hover:bg-black">
+          <li className="p-4 rounded-lg hover:bg-gray-400">
             <Link href="/#gallery">Gallery</Link>
           </li>{" "}
-          <li className="p-4 rounded-lg hover:bg-black">
+          <li className="p-4 rounded-lg hover:bg-gray-400">
             <Link href="/work">Work</Link>
           </li>{" "}
-          <li className="p-4 rounded-lg hover:bg-black">
+          <li className="p-4 rounded-lg hover:bg-gray-400">
             <Link href="/contact">Contact</Link>
           </li>
         </ul>
